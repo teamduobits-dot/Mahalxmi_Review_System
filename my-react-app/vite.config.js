7f531ad3d6ea862e50f2c0966fb3f680785d60e3
Aditya Gunde <team.duobits@gmail.com.com>
2026-09-08 03:06:49 +0530

    Deploy app to GitHub Pages

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -2,15 +2,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Mahalxmi_Review_System/',
+
   plugins: [react(), tailwindcss()],
+
   server: {
     host: true,
     port: 5173,
-    // Accept requests from any host so the app works behind preview proxies.
     allowedHosts: true,
   },
+
   preview: {
     host: true,
     port: 4173,
